97372241ba8968fcfa95997851df21c73a6eae57
Marcus Williams <marcus.williams@joipolloi.com>
2023-08-01 13:27:46 +0100

    Add talks to speakers

---
+++ components/layout/CardTalk/CardTalk.tsx
@@ -0,0 +1,43 @@
+import cn from 'classnames';
+
+import styles from './CardTalk.module.scss';
+import { NodeTalkCardFragment } from '@models/operations';
+import { Card, CardImage, CardContent, CardLink } from '../Card/Card';
+import { Text } from '@components/ui/Text/Text';
+
+export interface CardTalkProps {
+  /** Optional className for CardTalk, pass in a sass module class to override component default */
+  className?: string;
+  talk: NodeTalkCardFragment;
+  layout?: 'square' | 'landscape';
+}
+
+/**
+ * CardTalk description
+ */
+
+export const CardTalk = ({
+  className,
+  talk,
+  layout = 'landscape',
+}: CardTalkProps) => {
+  const rootClassName = cn(styles.root, className);
+  return (
+    <Card className={rootClassName}>
+      <CardImage
+        media={
+          layout === 'landscape'
+            ? talk.teaser.landscapeImage
+            : talk.teaser.squareImage
+        }
+      />
+      <CardContent>
+        <Text variant="h2">{talk.title}</Text>
+        <Text variant="body2">
+          {!!talk.speakers?.length ? talk.speakers[0].title : null}
+        </Text>
+      </CardContent>
+      <CardLink link={talk.path} />
+    </Card>
+  );
+};
--- components/node/NodeSpeaker/NodeSpeaker.tsx
+++ components/node/NodeSpeaker/NodeSpeaker.tsx
@@ -6,6 +6,7 @@ import { Text } from '@components/ui/Text/Text';
 import { RichText } from '@components/util/RichText/RichText';
 import { MediaImage } from '@components/media/MediaImage/MediaImage';
 import { TwoColumn } from '@components/layout/TwoColumn/TwoColumn';
+import { CardTalk } from '@components/layout/CardTalk/CardTalk';
 export interface NodeSpeakerProps {
   /** Optional className for NodeSpeaker, pass in a sass module class to override component default */
   className?: string;
@@ -19,34 +20,47 @@ export interface NodeSpeakerProps {
 export const NodeSpeaker = ({ className, node }: NodeSpeakerProps) => {
   const rootClassName = cn(styles.root, className);
   return (
-    <Container className={rootClassName} as="article">
+    <article className={rootClassName}>
       <header className={styles.header}>
         <MediaImage className={styles.headerImage} media={node.image} />
-        <Text variant="h1">{node.title}</Text>
-        {node.role ? (
-          <Text variant="body1" noMargin>
-            {node.role}
-          </Text>
-        ) : null}
-        {node.company ? (
-          <Text variant="body1" noMargin>
-            {node.company}
-          </Text>
-        ) : null}
-      </header>
-      <TwoColumn>
-        <TwoColumn.Content>
-          {node.summary?.value ? (
-            <Text variant="body1" as="h1">
-              {node.summary.value}
+        <Container className={styles.headerText}>
+          <Text variant="h1">{node.title}</Text>
+          {node.role ? (
+            <Text variant="body1" noMargin>
+              {node.role}
             </Text>
           ) : null}
-          {node.biography?.processed ? (
-            <RichText html={node.biography.processed} />
+          {node.company ? (
+            <Text variant="body1" noMargin>
+              {node.company}
+            </Text>
           ) : null}
-        </TwoColumn.Content>
-        <TwoColumn.Side>dsfdsg</TwoColumn.Side>
-      </TwoColumn>
-    </Container>
+        </Container>
+      </header>
+      <Container>
+        <TwoColumn>
+          <TwoColumn.Content>
+            {node.summary?.value ? (
+              <Text variant="body1" as="h1">
+                {node.summary.value}
+              </Text>
+            ) : null}
+            {node.biography?.processed ? (
+              <RichText html={node.biography.processed} />
+            ) : null}
+          </TwoColumn.Content>
+          <TwoColumn.Side>
+            {node.talks.__typename === 'TalksResult' &&
+            !!node.talks.results.length
+              ? node.talks.results.map((r) =>
+                  r.__typename === 'NodeTalk' ? (
+                    <CardTalk key={r.id} talk={r} />
+                  ) : null
+                )
+              : null}
+          </TwoColumn.Side>
+        </TwoColumn>
+      </Container>
+    </article>
   );
 };
