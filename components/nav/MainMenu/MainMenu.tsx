9e8849275977f29cb60388802890f8599c603a0e
Marcus Williams <marcus.williams@joipolloi.com>
2023-08-11 16:17:37 +0100

    Animate mobile menu

--- components/nav/MainMenu/MainMenu.tsx
+++ components/nav/MainMenu/MainMenu.tsx
@@ -2,6 +2,10 @@ import { MainMenuFragment } from '@models/operations';
 import cn from 'classnames';
 import { MenuList } from '../MenuList/MenuList';
 import styles from './MainMenu.module.scss';
+import { m } from 'framer-motion';
+import { menuAtom } from '@store/site';
+import { useAtom } from 'jotai';
+import { useMediaQuery } from '@hooks/useMediaQuery/useMediaQuery';
 
 export interface MainMenuProps {
   /** Optional className for MainMenu, pass in a sass module class to override component default */
@@ -10,10 +14,15 @@ export interface MainMenuProps {
 }
 
 export const MainMenu = ({ className, mainMenu }: MainMenuProps) => {
+  const [menuOpen] = useAtom(menuAtom);
+  const isTablet = useMediaQuery('(min-width: 910px)');
   const rootClassName = cn(styles.root, className);
   return (
-    <nav className={rootClassName}>
+    <m.nav
+      className={rootClassName}
+      animate={isTablet ? 'show' : !isTablet && menuOpen ? 'show' : 'hide'}
+    >
       {mainMenu?.items && <MenuList menus={mainMenu.items} type="main" />}
-    </nav>
+    </m.nav>
   );
 };
--- hooks/useMediaQuery/index.ts
+++ hooks/useMediaQuery/index.ts
@@ -1,2 +1,1 @@
-export * from './useScrollLock';
 export * from './useMediaQuery';
