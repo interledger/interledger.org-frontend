a70a5abf77a62a55468af3a5e9df0970bf096b7c
Marcus Williams <marcus.williams@joipolloi.com>
2023-08-15 11:23:36 +0100

    rename round

--- components/paragraph/ParagraphContentColumnCards/ParagraphContentColumnCards.tsx
+++ components/paragraph/ParagraphContentColumnCards/ParagraphContentColumnCards.tsx
@@ -5,14 +5,17 @@ import styles from './ParagraphContentColumnCards.module.scss';
 export interface ParagraphContentColumnCardsProps {
   /** Optional className for ParagraphContentColumnCards, pass in a sass module class to override component default */
   className?: string;
-  paragraph: ParagraphContentColumnCardsFragment
+  paragraph: ParagraphContentColumnCardsFragment;
 }
 
 /**
  * ParagraphContentColumnCards description
  */
 
-export const ParagraphContentColumnCards = ({ className }: ParagraphContentColumnCardsProps) => {
+export const ParagraphContentColumnCards = ({
+  className,
+  paragraph,
+}: ParagraphContentColumnCardsProps) => {
   const rootClassName = cn(styles.root, className);
-  return <div className={rootClassName}></div>;
+  return <section className={rootClassName}></section>;
 };
