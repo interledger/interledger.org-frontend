96ee0fed97b100dccf6c01538e000f06ba05f50b
Marcus Williams <marcus.williams@joipolloi.com>
2023-09-04 18:04:28 +0100

    fix yarn build issues

--- components/paragraph/ParagraphSpacer/ParagraphSpacer.stories.tsx
+++ components/paragraph/ParagraphSpacer/ParagraphSpacer.stories.tsx
@@ -1,6 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ParagraphSpacer } from './ParagraphSpacer';
-import { uniqueId } from 'lodash';
 
 const meta = {
   title: 'Paragraph/ParagraphSpacer',
@@ -14,7 +13,7 @@ export const Default: Story = {
   args: {
     paragraph: {
       __typename: 'ParagraphSpacer',
-      id: uniqueId(),
+      id: '6c7b8d2d-ffde-4614-8c0a-0abe1b81e8c1',
       spacerSize: 'small',
     },
   },
@@ -24,7 +23,7 @@ export const WithDivider: Story = {
   args: {
     paragraph: {
       __typename: 'ParagraphSpacer',
-      id: uniqueId(),
+      id: '6c7b8d2d-ffde-4614-8c0a-0abe1b81e8c1',
       spacerSize: 'small',
       spacerLine: true,
     },
